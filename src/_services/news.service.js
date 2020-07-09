b65e60b1dfa58f83782ea97f9c487728537a8658
Xinwen Zhang <xinwen.zhang.911@gmail.com>
2020-07-09 08:34:53 -0400

    Update news.service.js

--- src/_services/news.service.js
+++ src/_services/news.service.js
@@ -3,7 +3,7 @@ export const newsService = {
 };
 
 function getHeadlines() {
-    return fetch(`https://newsapi.org/v2/everything?q=stock&pageSize=100&sortBy=publishedAt&language=en&apiKey=d1012daa1a634325afa18f31a8d0899e`)
+    return fetch(`https://newsapi.org/v2/everything?q=stock&pageSize=100&sortBy=publishedAt&language=en&apiKey=7e29f33534d34fafb3aaed0ba7f4fae7`)
         .then(response =>
             response.json()
         )
