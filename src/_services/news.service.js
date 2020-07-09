91406f5dc0c2d65fc44622eadf4c4447be1ce034
xz <xinwen.zhang.911@gmail.com>
2020-07-09 09:01:31 -0400

    push

--- src/_services/news.service.js
+++ src/_services/news.service.js
@@ -1,11 +1,12 @@
+import  news  from "./news.json";
+
 export const newsService = {
     getHeadlines
 };
 
 function getHeadlines() {
-    return fetch(`https://newsapi.org/v2/everything?q=stock&pageSize=100&sortBy=publishedAt&language=en&apiKey=7e29f33534d34fafb3aaed0ba7f4fae7`)
-        .then(response =>
-            response.json()
-        )
+   return new Promise((resolve, reject) => {
+        resolve(news);
+      })
 }
 
