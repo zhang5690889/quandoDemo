2b874efb7a9b391938bc4e0324edbb38fec6fea6
XZ <xinwen.zhang.911@gmail.com>
2021-01-09 19:01:34 -0500

    updates demo msg

--- src/_components/DemoWarningBar.js
+++ src/_components/DemoWarningBar.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
 
 export class DemoWarningBar extends React.Component {
-    render() {
-        return (
-            <div className="alert alert-danger" role="alert" style={{textAlign: "center", fontWeight: "bold"}}>
-                You are in Demo Mode
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div
+        className="alert alert-danger"
+        role="alert"
+        style={{ textAlign: "center", fontWeight: "bold" }}
+      >
+        You are in demo mode. Contact{" "}
+        <a href="https://www.linkedin.com/in/xzhang-888/" target="_blank">
+          Victor Zhang
+        </a>{" "}
+        for more info!
+      </div>
+    );
+  }
 }
-
-
-
